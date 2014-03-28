024c9a543424b83754dd9597b16c979b9b5a7de9
Choonho Son <choonho.son@gmail.com>
2014-03-28 20:08:00 +0900

    Update initialize network device

--- dprobe.c
+++ dprobe.c
@@ -37,7 +37,9 @@
 #include <stdint.h>
 #include <errno.h>
 #include <sys/queue.h>
- 
+#include <getopt.h>
+
+#include <rte_common.h> 
 #include <rte_memory.h>
 #include <rte_memzone.h>
 #include <rte_launch.h>
@@ -46,9 +48,24 @@
 #include <rte_per_lcore.h>
 #include <rte_lcore.h>
 #include <rte_debug.h>
- 
+#include <rte_ethdev.h> 
 #include "dprobe.h"
+
+
+#define MBUF_SIZE (2048 + sizeof(struct rte_mbuf) + RTE_PKTMBUF_HEADROOM)
+#define NB_MBUF   8192
+
+
+/* mask of enabled ports */
+static uint32_t enabled_port_mask = 0;
+//static int promiscuous_on = 1; /* Ports set in promiscuous mode on by default */
  
+
+/* ethernet addresses ports */
+static struct ether_addr ports_eth_addr[RTE_MAX_ETHPORTS];
+static struct rte_mempool * pktmbuf_pool;
+
+
 static int
 lcore_probe(__attribute__((unused)) void *arg)
 {
@@ -57,17 +74,156 @@ lcore_probe(__attribute__((unused)) void *arg)
         printf("hello from core %u\n", lcore_id);
         return 0;
 }
- 
+
+
+static int
+parse_portmask(const char *portmask)
+{
+	char *end = NULL;
+	unsigned long pm;
+
+	/* parse hexadecimal string */
+	pm = strtoul(portmask, &end, 16);
+	if ((portmask[0] == '\0') || (end == NULL) || (*end != '\0'))
+		return -1;
+
+	if (pm == 0)
+		return -1;
+
+	return pm;
+}
+
+static void
+print_ethaddr(const char *name, const struct ether_addr *eth_addr)
+{
+	printf ("%s%02X:%02X:%02X:%02X:%02X:%02X", name,
+		eth_addr->addr_bytes[0],
+		eth_addr->addr_bytes[1],
+		eth_addr->addr_bytes[2],
+		eth_addr->addr_bytes[3],
+		eth_addr->addr_bytes[4],
+		eth_addr->addr_bytes[5]);
+}
+
+
+/* display usage */
+static void
+print_usage(const char *prgname)
+{
+	printf ("%s [EAL options] -- -p PORTMASK -P"
+		"  -p PORTMASK: hexadecimal bitmask of ports to configure\n"
+		"  -P : enable promiscuous mode\n",
+		prgname);
+}
+
+/* Parse the argument given in the command line of the application */
+static int
+parse_args(int argc, char **argv)
+{
+	int opt, ret;
+	char **argvopt;
+	int option_index;
+	char *prgname = argv[0];
+	static struct option lgopts[] = {
+		{NULL, 0, 0, 0}
+	};
+
+	argvopt = argv;
+
+	while ((opt = getopt_long(argc, argvopt, "p:",
+				  lgopts, &option_index)) != EOF) {
+
+		switch (opt) {
+		/* portmask */
+		case 'p':
+			enabled_port_mask = parse_portmask(optarg);
+			if (enabled_port_mask == 0) {
+				printf("invalid portmask\n");
+				print_usage(prgname);
+				return -1;
+			}
+			break;
+
+
+		/* long options */
+		case 0:
+			print_usage(prgname);
+			return -1;
+
+		default:
+			print_usage(prgname);
+			return -1;
+		}
+	}
+
+	if (optind >= 0)
+		argv[optind-1] = prgname;
+
+	ret = optind-1;
+	optind = 0; /* reset getopt lib */
+	return ret;
+}
+
+
+
 int
 MAIN(int argc, char **argv)
 {
         int ret;
         unsigned lcore_id;
+	unsigned nb_ports;
+	uint8_t portid;
  
         ret = rte_eal_init(argc, argv);
         if (ret < 0)
-                rte_panic("Cannot init EAL\n");
- 
+                rte_exit(EXIT_FAILURE, "Invalid EAL argument\n");
+
+	argc -= ret;
+	argv += ret;
+
+	/* parse application arguments (after the EAL ones) */
+	ret = parse_args(argc, argv);
+	if (ret < 0)
+		rte_exit(EXIT_FAILURE, "Invalid arguments\n");
+
+	/* create the mbuf pool */
+	pktmbuf_pool =
+		rte_mempool_create("mbuf_pool", NB_MBUF,
+					MBUF_SIZE, 32,
+					sizeof(struct rte_pktmbuf_pool_private),
+					rte_pktmbuf_pool_init, NULL,
+					rte_pktmbuf_init, NULL,
+					rte_socket_id(), 0);
+	/* init driver */
+	if (rte_pmd_init_all() < 0)
+		rte_exit(EXIT_FAILURE, "Cannot init pmd\n");
+
+	if (rte_eal_pci_probe() < 0)
+		rte_exit(EXIT_FAILURE, "Cannnot probe PCI\n");
+	
+	nb_ports = rte_eth_dev_count();
+	if (nb_ports > RTE_MAX_ETHPORTS)
+		nb_ports = RTE_MAX_ETHPORTS;
+
+	printf("Number of port:%d\n", nb_ports);
+
+	for (portid = 0; portid < nb_ports; portid++) {
+		if ((enabled_port_mask & (1 << portid)) == 0) {
+			printf("Skipping disabled port %d\n", portid);
+			continue;
+		}
+		rte_eth_macaddr_get(portid, &ports_eth_addr[portid]);
+		print_ethaddr("Address:", &ports_eth_addr[portid]);
+		printf("\n");	
+		
+		ret = rte_eth_dev_start(portid);
+		if (ret < 0)
+			rte_exit(EXIT_FAILURE, "rte_eth_dev_start: err=%d, port=%d\n", ret, portid);
+		printf("Enabled promiscuous mode : port %d\n", portid);
+		rte_eth_promiscuous_enable(portid);
+
+
+	}	
         /* call lcore_probe() on every slave lcore */
         RTE_LCORE_FOREACH_SLAVE(lcore_id) {
                 rte_eal_remote_launch(lcore_probe, NULL, lcore_id);
