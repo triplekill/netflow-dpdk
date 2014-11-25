a4e90cf43a6487b6f0f58f72492dc01b702a017c
root <root@cnode50-m.testbed.net>
2014-11-25 20:22:02 +0900

    Add BSD license

--- netflow-main.c
+++ netflow-main.c
@@ -1,8 +1,7 @@
 /*-
  *   BSD LICENSE
  * 
- *   Copyright(c) 2014 Choonho Son All rights reserved.
- *   choonho.son@gmail.com
+ *   Copyright(c) 2014, Choonho Son choonho.som@gmail.com
  *   All rights reserved.
  * 
  *   Redistribution and use in source and binary forms, with or without
@@ -15,9 +14,6 @@
  *       notice, this list of conditions and the following disclaimer in
  *       the documentation and/or other materials provided with the
  *       distribution.
- *     * Neither the name of Intel Corporation nor the names of its
- *       contributors may be used to endorse or promote products derived
- *       from this software without specific prior written permission.
  * 
  *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
